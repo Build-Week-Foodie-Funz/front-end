9f599747c2c78a663c5ba459cf31d1b4ad1286e4
Kate Efimova <kate.efimova0@gmail.com>
2019-09-27 14:55:18 -0700

    Fixing nav bar, in process

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,20 @@ import Logo from "../images/logo.svg";
 
 const NavContainer = styled.div`
   background-color: #db832d;
-  justify-content: flex-end;
+  justify-content: space-between;
   display: flex;
   align-items: center;
-  height: 50px;
+  height: 3em;
   width: 100%;
 `;
 
 const Navigation = styled.nav`
   dispaly: flex;
-  width: 100%;
+  width: 30%;
+  display: flex;
+  justify-content: space-around;
+  font-family: "Chica Mono";
+  background: red;
   @media (min-width: 500px) {
   }
   @media (min-width: 1200px) {
@@ -30,25 +34,36 @@ const NavItem = styled.a`
 `;
 
 const LogoImage = styled.img`
-  height: 40px;
-  margin-left: 1%;
+  height: 90%;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  width: 20em;
+  align-items: center;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  background: yellow;
 `;
 
 const LogoText = styled.a`
   display: flex;
-  justify-content: flex-end;
-  align-items: center;
   text-decoration: none;
   color: white;
-  font-size: 1.5rem;
+  font-size: 1.2em;
   // margin-right: 5%;
 `;
 
 const NavigationBar = () => {
   return (
     <NavContainer>
-
-      <LogoText> <LogoImage src={Logo} />Foodie Fun</LogoText>
+      <LogoContainer>
+        <LogoText>
+          <LogoImage src={Logo} />
+          <h2>Foodie Fun</h2>
+        </LogoText>
+      </LogoContainer>
       <Navigation>
         <NavItem href="/login">Login</NavItem>
         <NavItem href="https://foodiefun-marketing.netlify.com/">
